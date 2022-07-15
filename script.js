2044daca3e8ce64bb15a44cce3dfbf3b1b1ad34b
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-07-15 16:06:47 +0800

    Fixed grid alignment

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ content.classList.add('content');
 //container of grids, this is the big box
 
 let grid=1;
-let grindInput=16;
+let grindInput=64;
 let maxGrid=grindInput*grindInput;
 while (grid<=maxGrid){
     const box = document.createElement('div');
     box.classList.add('boxes');
-    box.textContent="⠀⠀";
+    
     content.appendChild(box);
     grid++;
 }
-content.style.gridTemplateColumns = `repeat(${grindInput}, 20px)`;
+content.style.gridTemplateColumns = `repeat(${grindInput}, 1fr)`;
 console.log("column i is "+grindInput)
-content.style.gridTemplateRows = `repeat(${grindInput}, 20px)`;
+content.style.gridTemplateRows = `repeat(${grindInput}, 1fr)`;
 
 
 container.appendChild(content);
